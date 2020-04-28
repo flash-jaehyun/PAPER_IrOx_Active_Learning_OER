c753a94c72dca3d2ca96ca3fdfe675e96c1f1947
Raul Flores <raulf2012@gmail.com>
2020-04-27 18:36:25 -0700

    BE ABLE TO COMPILE SEPARATE MAIN TEXT AND SI

--- scripts/clean_tex_files.sh
+++ scripts/clean_tex_files.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-# #############################################################################
+# #########################################################
 # Clean up temporary tex files produced from compiling the .tex manuscript
 #
 # Author(s): Raul A. Flores
-# #############################################################################
+# #########################################################
+
+#| - Help message
+if [ "$1" == "-h" ] ; then
+    # echo "Usage: `basename $0` [-h]"
+    echo "Usage:"
+    echo "clean_tex_files remove_pdf"
+    exit 0
+fi
+#__|
 
 cd "$(dirname "$0")"
-
 echo $PWD
 
+# #########################################################
+#| - Main tex file
 rm ../acs-00_main_manuscript.bib
 rm ../00_main_manuscript.synctex.gz
 
@@ -25,8 +35,38 @@ rm ../00_main_manuscript.fff
 rm ../00_main_manuscript.lof
 rm ../00_main_manuscript.ttt
 rm ../00_main_manuscript.lot
+#__|
 
+# #########################################################
+#| - SI tex file
+rm ../acs-00_SI.bib
+rm ../00_SI.synctex.gz
+
+rm ../00_SI.bbl
+rm ../00_SI.blg
+
+rm ../00_SI.aux
+rm ../00_SI.log
+
+rm ../00_SI.synctex\(busy\)
+
+rm ../00_SI.fff
+rm ../00_SI.lof
+rm ../00_SI.ttt
+rm ../00_SI.lot
+#__|
+
+
+if [ "$1" == "remove_pdf" ]; then
+  rm ../00_SI.pdf
+  rm ../00_main_manuscript.pdf
+fi
+
+
+
+#| - __old__
 # currentDate=`date +"%Y%m%d_%H-%m"`
 # filename="_00_main_manuscript.pdf"
 # echo "$currentDate$filename"
 # mv ../00_main_manuscript.pdf ../__old__/"$currentDate$filename"
+#__|
--- scripts/compile_tex.sh
+++ scripts/compile_tex.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
 
-# #############################################################################
+# #########################################################
 # Clean up temporary tex files produced from compiling the .tex manuscript
 #
 # Author(s): Raul A. Flores
-# #############################################################################
+# #########################################################
+
+#| - Help message
+
+help_message () {
+  echo "***********************************************************"
+  echo "Usage:"
+  echo "  ./scripts/compile_tex.sh 00_main_manuscript [option]"
+  echo "or"
+  echo "  ./scripts/compile_tex.sh separate_full"
+
+  echo ""
+
+  echo "Options:"
+  echo "  short"
+  echo "  full"
+  echo "  separate_full"
+}
+
+
+if [ "$1" == "-h" ] ; then
+    help_message
+
+    exit 0
+fi
+#__|
+
 
 # CWD should always be root git dir
 cd "$(dirname "$0")"
@@ -25,50 +51,89 @@ print_stars () {
 
 pdflatex_comm () {
   # pdflatex.exe \
-  pdflatex -synctex=1 -interaction=nonstopmode -file-line-error 00_main_manuscript
   # pdflatex.exe -synctex=1 -interaction=nonstopmode -file-line-error 00_main_manuscript
+
+  # pdflatex -synctex=1 -interaction=nonstopmode -file-line-error 00_main_manuscript
+  pdflatex -synctex=1 -interaction=nonstopmode -file-line-error $1
+
 }
 
 bibtex_comm () {
-  bibtex 00_main_manuscript
+  # bibtex 00_main_manuscript
+  bibtex $1
 }
 
 full_scheme () {
-  pdflatex_comm
+  pdflatex_comm $1
   print_stars
 
-  bibtex_comm
+  bibtex_comm $1
   print_stars
 
-  pdflatex_comm
+  pdflatex_comm $1
   print_stars
 
-  pdflatex_comm
+  pdflatex_comm $1
 }
 #__|
 
-print_stars
-if [ "$1" == "short" ]; then
+echo "Printing arguments"
+echo $1
+echo $2
+
+#| - Processing arguments
+if [ "$1" == "00_main_manuscript.tex" ]; then
+  tex_file="00_main_manuscript"
+elif [ "$1" == "00_SI.tex" ]; then
+  tex_file="00_SI"
+else
+  tex_file=$1
+fi
+#__|
+
+if [ "$2" == "short" ]; then
   #| - short
   echo "Running short compilation scheme (just 1 pdflatex call)"
   echo ""; echo ""
 
-  pdflatex_comm
+  print_stars
+  # pdflatex_comm $1
+  pdflatex_comm $1
   print_stars
   #__|
-elif [ "$1" == "full" ]; then
+elif [ "$2" == "full" ]; then
   #| - full
   echo "Running full compilation (pdflatex > bibtex > pdflatex > pdflatex)"
   echo ""; echo ""
 
-  full_scheme
+  print_stars
+  # full_scheme $1
+  # echo "OKLSJFJSDJFISDIFISDIFISODIFJSD"
+  # echo $tex_file
+  full_scheme $tex_file
+  print_stars
+  #__|
+elif [ "$1" == "separate_full" ]; then
+  #| - full
+  echo "Running full compilation scheme to to generate separate main text and SI"
+  echo "First we run both 00_main_manuscript and 00_SI, then we run them both again to that proper linking occurs, finall we we clean everything up"
+
+  print_stars
+
+  full_scheme 00_main_manuscript
+  full_scheme 00_SI
+
+  full_scheme 00_main_manuscript
+  full_scheme 00_SI
+
+  echo $PWD
+  ./scripts/clean_tex_files.sh
+
+  print_stars
   #__|
 else
   #| - else
-  echo "Run script with either of these options:"
-  echo "'short'"
-  echo "'full'"
-  echo ""; echo ""
+  echo "Need to give an argument, choose among these options"
+  help_message
   #__|
 fi
-print_stars
