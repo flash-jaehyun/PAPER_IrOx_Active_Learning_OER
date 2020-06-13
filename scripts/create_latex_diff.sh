1a6fd6158f8c80df74b797d775d45cba34d58a71
Raul Flores <raulf2012@gmail.com>
2020-06-12 17:14:37 -0700

    Update create_latex diff to crate main and SI versions at same time

--- scripts/create_latex_diff.sh
+++ scripts/create_latex_diff.sh
@@ -1,7 +1,20 @@
-# TEMP
+#!/bin/bash
 
+# #########################################################
+# Create diff latex docs between current state and v2.0
+# #########################################################
 
 latexdiff \
-    $gr/PAPER_IrOx_Active_Learning_OER-2.0/PAPER_IrOx_Active_Learning_OER-2_collated.tex \
+    $gr/PAPER_IrOx_Active_Learning_OER-2.0/PAPER_IrOx_Active_Learning_OER-2.0/00_SI_collated.tex \
+    00_SI_collated.tex \
+    > diff_SI.tex
+
+latexdiff \
+    $gr/PAPER_IrOx_Active_Learning_OER-2.0/PAPER_IrOx_Active_Learning_OER-2.0/00_main_manuscript_collated.tex \
     00_main_manuscript_collated.tex \
-    > diff.tex
+    > diff_main_manuscript.tex
+
+# latexdiff \
+#     $gr/PAPER_IrOx_Active_Learning_OER-2.0/PAPER_IrOx_Active_Learning_OER-2_collated.tex \
+#     00_main_manuscript_collated.tex \
+#     > diff.tex
